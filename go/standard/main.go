a09aee882300108a979d59b328aa2153b28c42ae
David Moore <davemooreuws@gmail.com>
2025-10-01 15:41:39 +1000

    fix: ensure go template is consistent with others

--- go/standard/main.go
+++ go/standard/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"example/suga"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -17,14 +18,39 @@ func main() {
 	}
 
 	// setup a basic http server
-	router.HandleFunc("GET /hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("GET /read/{name}", func(w http.ResponseWriter, r *http.Request) {
 		name := r.PathValue("name")
 
-		// Use the suga client to access cloud resources
-		app.Files.Write("example.txt", []byte("Hello, "+name+"!"))
+		contents, err := app.Image.Read(name)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"detail": "%s"}`, err.Error()), http.StatusInternalServerError)
+			return
+		}
 
+		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, " + name + "!"))
+		w.Write(contents)
+	})
+
+	router.HandleFunc("POST /write/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := r.PathValue("name")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"detail": "%s"}`, err.Error()), http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		err = app.Image.Write(name, body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"detail": "%s"}`, err.Error()), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("File '%s' written to bucket.", name)))
 	})
 
 	port := os.Getenv("PORT")
